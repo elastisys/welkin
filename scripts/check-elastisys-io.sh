2092930b73a0748d36f8d3486233c417c6fb1de1
Cristian Klein <cristian.klein@elastisys.com>
2025-09-15 13:21:40 +0200

    Various fixes

--- scripts/check-elastisys-io.sh
+++ scripts/check-elastisys-io.sh
@@ -3,4 +3,4 @@
 # See https://github.com/lycheeverse/lychee/issues/1819
 curl -s https://elastisys.io/welkin/sitemap.xml \
   | grep -oP '(?<=<loc>).*?(?=</loc>)' \
-  | lychee --files-from -
+  | xargs lychee
